51fc1004d8e82e366f15776f8191dda44af33743
dlrkdgml0716 <dlrkdgml3737@naver.com>
2025-12-31 00:04:32 +0900

    commit

--- Level_1/성격 유형 검사기/src/Solution.java
+++ Level_1/성격 유형 검사기/src/Solution.java
@@ -1,2 +1,29 @@
-//public class Solution {
-//}
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        StringBuilder answer = new StringBuilder();
+        HashMap<Character, Integer> map = new HashMap<>();
+        char[] chrs = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+        for (char c : chrs) {
+            map.put(c, 0);
+        }
+
+        for (int i = 0; i < survey.length; i++) {
+            int score = choices[i];
+            if (score < 4) {
+                char ch = survey[i].charAt(0);
+                map.put(ch, map.get(ch) + (4 - score));
+            } else if (score > 4) {
+                char ch = survey[i].charAt(1);
+                map.put(ch, map.get(ch) + (score - 4));
+            }
+        }
+        answer.append(map.get('R') >= map.get('T') ? 'R' : 'T');
+        answer.append(map.get('C') >= map.get('F') ? 'C' : 'F');
+        answer.append(map.get('J') >= map.get('M') ? 'J' : 'M');
+        answer.append(map.get('A') >= map.get('N') ? 'A' : 'N');
+
+        return answer.toString();
+    }
+}
