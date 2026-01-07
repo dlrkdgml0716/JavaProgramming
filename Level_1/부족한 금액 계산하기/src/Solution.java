cda81867fdbf769932846999e61663950c36d594
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-01-07 09:44:11 +0900

    commit

--- Level_1/부족한 금액 계산하기/src/Solution.java
+++ Level_1/부족한 금액 계산하기/src/Solution.java
@@ -1,2 +1,16 @@
-public class Solution {
+import java.lang.Math;
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = money;
+        long total = 0;
+        for(int i = 1; i <= count; i++){
+            total += price * i;
+        }
+        if(answer - total > 0){
+            return 0;
+        }
+        else{
+            return Math.abs(answer-total);
+        }
+    }
 }
