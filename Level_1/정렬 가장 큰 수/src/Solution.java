f752953bd22a061dd5746f0ac74d636f4435ea6b
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-01-10 15:10:18 +0900

    commit

--- Level_1/정렬 가장 큰 수/src/Solution.java
+++ Level_1/정렬 가장 큰 수/src/Solution.java
@@ -21,3 +21,7 @@ class Solution {
         return answer.toString();
     }
 }
+
+// Arrays. sort의 조건을 (a, b) -> (b + a).compareTo(a + b) 하여 내림차순으로 정렬
+// compareTo는 사전순(유니코드) 값으로 비교, 만약 b+a가 a+b보다 크다면 b가 a보다 큼 -> 정수를 반환하여 a, b 순서를 바꿈
+// 음수나 0을 반환하면 순서 유지
