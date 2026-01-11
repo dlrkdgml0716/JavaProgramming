a13be24945b839edcd6287559bad1af9561bfce8
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-01-11 15:41:16 +0900

    commit

---
+++ Level_1/완전범죄/src/Solution.java
@@ -0,0 +1,33 @@
+    import java.util.Arrays;
+
+    class Solution {
+        public int solution(int[][] info, int n, int m) {
+            int[] dp = new int[m];
+            int INF = 1000000;
+            Arrays.fill(dp, INF);
+            dp[0] = 0;
+
+            for (int[] item : info) {
+                int aTrace = item[0];
+                int bTrace = item[1];
+                int[] nextDp = new int[m];
+                Arrays.fill(nextDp, INF);
+
+                for (int j = 0; j < m; j++) {
+                    if (dp[j] == INF) continue;
+                    if (dp[j] + aTrace < n) {
+                        nextDp[j] = Math.min(nextDp[j], dp[j] + aTrace);
+                    }
+                    if (j + bTrace < m) {
+                        nextDp[j + bTrace] = Math.min(nextDp[j + bTrace], dp[j]);
+                    }
+                }
+                dp = nextDp;
+            }
+            int answer = INF;
+            for (int i = 0; i < m; i++) {
+                answer = Math.min(answer, dp[i]);
+            }
+            return (answer == INF) ? -1 : answer;
+        }
+    }
--- Level_1/정렬 H-Index/src/Solution.java
+++ Level_1/정렬 H-Index/src/Solution.java
@@ -1,17 +1,36 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
-        for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;
+    public int solution(int[][] info, int n, int m) {
+        int[] dp = new int[m]; //
+        int INF = 1000000; // 최솟값을 찾기위한 적당한 큰수
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
 
-            if (citations[i] >= h) {
-                answer = h;
-                break;
+        for (int[] item : info) {
+            int aTrace = item[0];
+            int bTrace = item[1];
+
+            int[] nextDp = new int[m];
+            Arrays.fill(nextDp, INF);
+
+            for (int j = 0; j < m; j++) {
+                if (dp[j] == INF) continue;
+                if (dp[j] + aTrace < n) {
+                    nextDp[j] = Math.min(nextDp[j], dp[j] + aTrace);
+                }
+                if (j + bTrace < m) {
+                    nextDp[j + bTrace] = Math.min(nextDp[j + bTrace], dp[j]);
+                }
             }
+            dp = nextDp;
         }
-        return answer;
+        int answer = INF;
+        for (int i = 0; i < m; i++) {
+            answer = Math.min(answer, dp[i]);
+        }
+        return (answer == INF) ? -1 : answer;
     }
 }
+
+// 못풀었음
