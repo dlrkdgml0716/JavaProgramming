4ab677f2a80b3180d6ed824b095dcbed76c6e427
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-01-22 20:05:49 +0900

    commit

---
+++ Level_2/스택-큐 올바른 괄호/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                st.push('(');
+            } else if (c == ')') {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
