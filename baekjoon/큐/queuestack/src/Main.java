af04f7bf457c85870d4719f2100453f52567268d
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-02-05 12:44:28 +0900

    commit

---
+++ baekjoon/큐/queuestack/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] type = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            type[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            if (type[i] == 0) {
+                deque.addLast(value);
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            int nextVal = Integer.parseInt(st.nextToken());
+            deque.addFirst(nextVal);
+            sb.append(deque.pollLast()).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
