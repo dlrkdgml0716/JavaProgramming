8d50857db35ce084796b1bbc2405c4da595cdf6c
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-02-22 14:30:51 +0900

    commit

---
+++ baekjoon/큐/N번째 큰 수/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                queue.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for(int i = 0; i < N -1; i++) queue.poll();
+        System.out.print(queue.poll());
+    }
+}
