f7e721ec9787c02d512a0cdcbe07a8a10e20ed97
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-05-17 20:49:29 +0900

    commit

---
+++ Level_3/등굣길/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] map = new int[n+1][m+1];
+        for(int[] puddle : puddles){
+            map[puddle[1]][puddle[0]] = -1;
+        }
+        map[1][1] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(i == 1 && j == 1) continue;
+                if(map[i][j] == -1){
+                    map[i][j] = 0;
+                    continue;
+                }
+
+                int up = map[i-1][j];
+                int left = map[i][j-1];
+
+                map[i][j] = (up + left) % 1000000007;
+            }
+        }
+        return map[n][m];
+    }
+}
