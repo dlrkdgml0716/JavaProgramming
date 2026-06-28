638180b37ab97e0628483136bfdb578f061c721d
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-06-28 16:51:24 +0900

    commit

---
+++ codeUp/백트래킹/극장 좌석 배치 1/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    static int count;
+    public static void main(String[] args) {
+        count = 0;
+        Scanner sc = new Scanner(System.in);
+        int seats = sc.nextInt();
+        int people = sc.nextInt();
+        solved(seats, people, 0, 0);
+        System.out.print(count);
+    }
+    public static void solved(int seats, int people, int currentS, int currentP){
+        if(currentP == people){
+            count++;
+            return;
+        }
+        for(int i = currentS; i <= seats - people + currentP; i++){
+            solved(seats, people, i + 1, currentP + 1);
+        }
+    }
+}
