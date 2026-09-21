870bd0c7e0f72fb4fbf3664b09f16be77c140280
dlrkdgml0716 <dlrkdgml3737@naver.com>
2026-09-21 18:25:50 +0900

    commit

---
+++ codeUp/dynamic programming/합의 개수/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long pow = 1;
+        for (int i = 0; i < n - 1; i++) {
+            pow *= 2;
+        }
+        System.out.print(pow-1);
+    }
+}
